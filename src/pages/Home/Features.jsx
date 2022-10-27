95cf69fcf90b248903e7581816f5f12b6db7c880
aminulislam01101 <aminul.islam01101@gmail.com>
2022-10-27 22:48:59 +0600

    testimonial added in home page

---
+++ src/pages/Home/Features.jsx
@@ -0,0 +1,117 @@
+/* eslint-disable react/self-closing-comp */
+import React from 'react';
+
+const Features = () => (
+    <div>
+        <section className="dark:bg-gray-800 dark:text-gray-100 bg-slate-300 shadow-lg">
+            <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+                <div>
+                    <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl dark:text-gray-50">
+                        At What we are best
+                    </h2>
+                </div>
+                <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                    <div>
+                        <div className="mt-12 space-y-12">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            className="w-7 h-7"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M5 13l4 4L19 7"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                                        24 Hours Supports Systems
+                                    </h4>
+                                    <p className="mt-2 dark:text-gray-400">
+                                        Student get 24 hours Supports from us. We are simply a knock
+                                        away.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            className="w-7 h-7"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M5 13l4 4L19 7"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                                        Updated Course
+                                    </h4>
+                                    <p className="mt-2 dark:text-gray-400">
+                                        Our learners will get Latest course update whatever the
+                                        technology or personal development
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            className="w-7 h-7"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M5 13l4 4L19 7"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                                        Life time access
+                                    </h4>
+                                    <p className="mt-2 dark:text-gray-400">
+                                        Once a learner of ours, forever will be the owners.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div aria-hidden="true" className="mt-10 lg:mt-0">
+                        <img
+                            src="https://img.freepik.com/free-photo/financial-charts-table-with-laptop_1232-2736.jpg?w=996&t=st=1666888186~exp=1666888786~hmac=956002a63c898853e20379eadb5d5a1ece33eeb76c7a442804bfc83d434b2428"
+                            alt=""
+                            className="mx-auto rounded-lg shadow-lg dark:bg-gray-500"
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+);
+
+export default Features;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import Features from './Features';
+import Testimonials from './Testimonials';
 
 export default function Home() {
     return (
-        <div>
+        <div className="space-y-20">
             <Banner />
+            <Features />
+            <Testimonials />
         </div>
     );
 }
