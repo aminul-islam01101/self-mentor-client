6d94c72f60ae095266c7ad4266456d8b23717efa
MD. AMINUL ISLAM <aminul.islam01101@gmail.com>
2022-12-22 15:56:04 +0600

    Update ProtectedRoute.jsx

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation();
     if (loading) {
         return (
-            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400" />
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-sky-700" />
         );
     }
 
